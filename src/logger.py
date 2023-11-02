27d2120eff25799494a217e252a9cffbc673e8a9
Akash Singh <singhakashcu@gmail.com>
2023-11-03 00:16:16 +0530

    commit

--- src/logger.py
+++ src/logger.py
@@ -10,7 +10,7 @@ log_file_path = os.path.join(log_folder, log_file)
 
 logging.basicConfig(
     filename=log_file_path,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Fixed typo in 'levelname'
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
